b67e1f5dd78d23bbdfd3e89e01c3e9b839a81ef8
sai8charan <65341947+sai8charan@users.noreply.github.com>
2023-04-09 23:40:36 +0530

    styling & letters animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,25 +1,39 @@
 import './index.scss';
 import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['a','i',' ', 'C','h','a','r','a','n'];
+    const jobArray = ['F','u','l','l', ' ', 'S','t','a','c','k',' ','J','a','v','a',' ','D','e','v','e','l','o','p','e','r'];
 
     return (
 
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                    Hi,
-                    <br />
-                    I'm
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
+                    <br/>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
                     <img src={LogoTitle} alt="developer" srcset="" />
-                    Charan
+                    <AnimatedLetters letterClass={letterClass}
+                                     strArray={nameArray}
+                                     idx={15} />
+                   <br/>
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={24} />
                     <br />
-                    Full Stack Java Developer
+                    <br/>
                 </h1>
                 <h2>
                     Java Developer / Learning Frontend /
-                    <Link to="/contact" className='flat-botton'>CONTACT ME</Link> 
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link> 
                 </h2>
             </div>
         </div>
